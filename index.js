670df9825786c8fd6408b9f87b58b589dfa65f96
jakiadp <jakiadp@gmail.com>
2025-10-07 04:42:23 +0600

    ad to get

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const cors = require('cors')
 const app = express();
 const port = process.env.PORT || 3000;
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
 
 
@@ -42,6 +42,15 @@ async function run() {
         const cursor = jobCollection.find();
         const result = await cursor.toArray();
         res.send(result);
+    })
+
+    // job id
+
+    app.get('/jobs/:id',async(req, res)=>{
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)}
+      const result = await jobCollection.findOne(query)
+      res.send(result)
     })
 
     // Send a ping to confirm a successful connection
